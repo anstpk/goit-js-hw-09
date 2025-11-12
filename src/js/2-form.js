5d633f0b043b3de1561327447bf9769046f8e33d
anstpk <anastasiia.stupka@gmail.com>
2025-11-12 17:34:46 +0200

    9th js homework

---
+++ src/js/2-form.js
@@ -0,0 +1,66 @@
+function saveToLS (key, value) {
+  const zip = JSON.stringify(value);
+  localStorage.setItem(key, zip);
+}
+
+function loadFromLS(key) {
+  const zip = localStorage.getItem(key);
+
+  try {
+    const value = JSON.parse(zip);
+    return value;
+  } catch {
+    return zip; 
+  }
+}
+
+const form = document.querySelector('.form')
+
+const formData = {
+  email: "", 
+  message: ""
+}
+
+const STORAGE_KEY = 'feedback-form-state';
+
+form.addEventListener('input', (evt) => {
+  if (evt.target === form.elements.email) {
+    formData.email = evt.target.value;
+  } else if (evt.target === form.elements.message) {
+    formData.message = evt.target.value;
+  } 
+
+  saveToLS (STORAGE_KEY, formData);
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const savedData = loadFromLS(STORAGE_KEY);
+
+  if (savedData) {
+    formData.email = savedData.email;
+    formData.message = savedData.message;
+  } 
+
+  form.elements.email.value = formData.email;
+  form.elements.message.value = formData.message;
+
+});
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  if (formData.email === '' || formData.message === '') {
+    alert (`Fill please all fields`);
+    return;
+  }
+
+  console.log(formData);
+
+  localStorage.removeItem(STORAGE_KEY);
+  
+  form.elements.email.value = '';
+  form.elements.message.value = '';
+
+  formData.email = '';
+  formData.message = '';
+})
